80b4b0d719def8cb26ad405209f5fa40937417ee
Mariano <marianograssolifia@gmail.com>
2015-05-22 17:26:14 -0300

    avance sobre la ABM

--- controllers/post.php
+++ controllers/post.php
@@ -1,10 +1,28 @@
 <?php
 	
-	session_start();
+	//session_start();
 	
 		require_once ("../modules/conexion.php");
 		include ("../modules/Twig/twig.php");
+		include ("../models/post.php");
 
-
+		if ( (isset($_POST['titulo'])) && (isset($_POST['subtitulo'])) && (isset($_POST['descripcion'])) ){
+			
+			$titulo=$_POST['titulo'];
+			$subtitulo=$_POST['subtitulo'];
+			$desc=$_POST['descripcion'];
+			$fecha=date('Y-m-d');
+			if ((isset ($_POST['tags']))){
+				$tags=$_POST['tags'];
+			}
+			else {
+				$tags=null;
+			}
+			Post::agregar($titulo,$subtitulo,$desc,$tags,$fecha);
+			header('Location: ../controllers/asd.php');
+		}
+		else{
+			$twig->display('post.html');
+		}
 
 ?>
--- models/post.php
+++ models/post.php
@@ -9,7 +9,7 @@ class Post{
 	private $tags;
 	private	$post_date;    
 	
-	public function agregar($titulo,$subtitulo,$descripcion,$tags) 
+	public function agregar($titulo,$subtitulo,$descripcion,$tags,$post_date) 
 	{  
 	    $db = conectar();
 	    $stmt = $db->prepare("INSERT INTO post (titulo, subtitulo, descripcion, tags, post_date) VALUES(:titulo, :subtitulo, :descripcion, :tags, :post_date)");
